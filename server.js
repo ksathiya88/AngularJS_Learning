995eca1dde29afaafee2ecb1789a011f0f9b29e0
Karthik S <37337298+ksathiya88@users.noreply.github.com>
2019-05-29 22:13:23 +0530

    Fixing error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
     //var mongoose = require('mongoose');                     // mongoose for mongodb
     var morgan = require('morgan');             // log requests to the console (express4)
     var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+    //var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
     // configuration =================
 
@@ -53,9 +53,14 @@ const employees= [
     app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
     app.use(bodyParser.json());                                     // parse application/json
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-    app.use(methodOverride());
 
-    
+
+   app.use(function(req, res, next) {
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     next();
+   });
+
    app.get('/getEmployees', function(req, res){
       console.log("employees",employees);
       res.send(employees);
